f1c8832b981a2cc0d683c92298df96acae406e26
Michael Rose <est.michael@gmail.com>
2018-01-16 14:40:11 -0500

    Start again

---
+++ data/SiteConfig.js
@@ -0,0 +1,37 @@
+module.exports = {
+  imagesDir: "images", // Name of the directory that contains your images.
+  blogPostDir: "posts", // Name of the directory that contains your posts.
+  siteTitle: "Gatsby Test", // Site title.
+  siteTitleAlt: "Jekyll to Gatsby test", // Alternative site title for SEO.
+  siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
+  siteUrl: "", // Domain of your website without pathPrefix.
+  // pathPrefix: "/gatsby-advanced-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteDescription: "A GatsbyJS site migrated from Jekyll.", // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml", // Path to the RSS file.
+  // siteFBAppID: "", // FB Application ID for using app insights
+  // googleAnalyticsID: "", // GA tracking ID.
+  // disqusShortname: "", // Disqus shortname.
+  // postDefaultCategoryID: "tech", // Default category for posts.
+  userName: "Default User", // Username to display in the author segment.
+  userTwitter: "mmistakes", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "North Pole, Earth", // User location to display in the author segment.
+  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
+  userDescription:
+    "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
+  // Links to social profiles/projects you want to display in the author segment/navigation bar.
+  userLinks: [
+    {
+      label: "GitHub",
+      url: "https://github.com/mmistakes",
+      iconClassName: "fa fa-github"
+    },
+    {
+      label: "Twitter",
+      url: "https://twitter.com/mmistakes",
+      iconClassName: "fa fa-twitter"
+    }
+  ],
+  copyright: "Copyright © 2018.", // Copyright string for the footer of the website and RSS feed.
+  themeColor: "#c62828", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#e0e0e0" // Used for setting manifest background color.
+};
