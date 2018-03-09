13a3d74e4469ec1b04b5bd5fcde91941cb0c4eb8
Michael Rose <est.michael@gmail.com>
2018-03-09 16:08:49 -0500

    Cleanup `createPages`

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,92 +18,96 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
   }
 };
 
-const createTagPages = (createPage, edges) => {
-  // Tell it to use our tags template.
-  const tagTemplate = path.resolve(`src/templates/tags.js`);
-  // Create an empty object to store the posts.
-  const posts = {};
-  console.log("creating posts");
-
-  // Loop through all nodes (our markdown posts) and add the tags to our post object.
-
-  edges.forEach(({ node }) => {
-    if (node.frontmatter.tags) {
-      node.frontmatter.tags.forEach(tag => {
-        if (!posts[tag]) {
-          posts[tag] = [];
-        }
-        posts[tag].push(node);
-      });
-    }
-  });
-
-  // Create the tags page with the list of tags from our posts object.
-  createPage({
-    path: "/tags",
-    component: tagTemplate,
-    context: {
-      posts
-    }
-  });
-
-  // For each of the tags in the post object, create a tag page.
-
-  Object.keys(posts).forEach(tagName => {
-    const post = posts[tagName];
-    tagSlug = slugify(tagName, { lower: true });
-    createPage({
-      path: `/tags/${tagSlug}`,
-      component: tagTemplate,
-      context: {
-        posts,
-        post,
-        tag: tagName
-      }
-    });
-  });
-};
-
 exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators;
+
   return new Promise((resolve, reject) => {
-    graphql(`
-      {
-        allMarkdownRemark {
-          edges {
-            node {
-              fields {
-                slug
-                date
+    // Set templates.
+    const postTemplate = path.resolve(`./src/templates/post.js`);
+    const tagsTemplate = path.resolve(`./src/templates/tags.js`);
+
+    resolve(
+      graphql(
+        `
+          {
+            allMarkdownRemark {
+              edges {
+                node {
+                  fields {
+                    slug
+                    date
+                  }
+                  frontmatter {
+                    title
+                    categories
+                    tags
+                  }
+                  excerpt
+                }
               }
-              frontmatter {
-                title
-                categories
-                tags
-              }
-              excerpt
             }
           }
-        }
-      }
-    `).then(result => {
-      const posts = result.data.allMarkdownRemark.edges;
+        `
+      ).then(result => {
+        const posts = result.data.allMarkdownRemark.edges;
 
-      // call createTagPages with the result of posts
-      createTagPages(createPage, posts);
+        const createTagPages = (createPage, edges) => {
+          // Create an empty object to store the posts.
+          const posts = {};
+          console.log("creating posts");
 
-      result.data.allMarkdownRemark.edges.map(({ node }) => {
-        createPage({
-          path: node.fields.slug,
-          component: path.resolve(`./src/templates/blog-post.js`),
-          context: {
-            // Data passed to context is available in page queries as GraphQL variables.
-            slug: node.fields.slug,
-            date: node.fields.date
-          }
+          // Loop through all nodes (Markdown posts) and add the tags to post object.
+          edges.forEach(({ node }) => {
+            if (node.frontmatter.tags) {
+              node.frontmatter.tags.forEach(tag => {
+                if (!posts[tag]) {
+                  posts[tag] = [];
+                }
+                posts[tag].push(node);
+              });
+            }
+          });
+
+          // Create tags page listing from posts object.
+          createPage({
+            path: "/tags",
+            component: tagsTemplate,
+            context: {
+              posts
+            }
+          });
+
+          // For each tag in the post object, create a tag page.
+          Object.keys(posts).forEach(tagName => {
+            const post = posts[tagName];
+            tagSlug = slugify(tagName, { lower: true });
+            createPage({
+              path: `/tags/${tagSlug}`,
+              component: tagsTemplate,
+              context: {
+                posts,
+                post,
+                tag: tagName
+              }
+            });
+          });
+        };
+
+        // Call createTagPages with the result of posts.
+        createTagPages(createPage, posts);
+
+        result.data.allMarkdownRemark.edges.map(({ node }) => {
+          createPage({
+            path: node.fields.slug,
+            component: postTemplate,
+            context: {
+              // Data passed to context is available in page queries as GraphQL variables.
+              slug: node.fields.slug,
+              date: node.fields.date
+            }
+          });
         });
-      });
-      resolve();
-    });
+      })
+    );
   });
 };
