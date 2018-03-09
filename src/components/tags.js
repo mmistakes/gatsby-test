3ecd01ea14fd27bb5a899ee74b4ddc4b4fe20246
Michael Rose <est.michael@gmail.com>
2018-03-09 12:30:18 -0500

    Fix tag links
    - Lowercase and replace spaces with `-` using kebabcase

--- src/components/tags.js
+++ src/components/tags.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Link from "gatsby-link";
+import kebabCase from "lodash/kebabcase";
+
 
 export default function Tags({ list = [] }) {
   return (
     <ul className="tag-list">
       {list.map(tag => (
         <li key={tag}>
-          <Link to={`/tags/${tag}`}>{tag}</Link>
+          <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
         </li>
       ))}
     </ul>
