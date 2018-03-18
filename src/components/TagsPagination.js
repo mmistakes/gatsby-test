4c82cd166ee277b1a9bd667f8436e4244b934696
Michael Rose <est.michael@gmail.com>
2018-03-17 22:25:59 -0400

    Normalize tags with spaces

--- src/components/TagsPagination.js
+++ src/components/TagsPagination.js
@@ -1,18 +1,19 @@
 import React from "react";
 import Link from "gatsby-link";
+import kebabCase from "lodash/kebabcase";
 import PropTypes from "prop-types";
 
 const Pagination = ({ tag, page, pagesSum }) => (
   <nav>
     {page === 2 && (
-      <Link to={`/tag/${tag}/`}>Newer Posts</Link>
+      <Link to={`/tag/${kebabCase(tag)}/`}>Newer Posts</Link>
     )}
     {page > 2 && (
-      <Link to={`/tag/${tag}/page/${page - 1}/`}>Newer Posts</Link>
+      <Link to={`/tag/${kebabCase(tag)}/page/${page - 1}/`}>Newer Posts</Link>
     )}
     <span>{`Page ${page} of ${pagesSum}`}</span>
     {page < pagesSum && (
-      <Link to={`/tag/${tag}/page/${page + 1}/`}>Older Posts</Link>
+      <Link to={`/tag/${kebabCase(tag)}/page/${page + 1}/`}>Older Posts</Link>
     )}
   </nav>
 );
