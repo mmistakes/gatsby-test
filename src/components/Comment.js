af8b0f0111bf1df906b6c89d9b191b1fa075dc5e
Michael Rose <est.michael@gmail.com>
2018-04-27 10:11:03 -0400

    Add basic comment styling

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import Gravatar from 'react-gravatar'
+import { rhythm, scale } from '../utils/typography'
 
 const Comment = props => {
   const { name, url, email, friendlyDate, iso8601Date, children } = props
 
   return (
-    <article>
-      <div>
+    <article
+      style={{
+        display: 'flex',
+        marginBottom: rhythm(1),
+      }}
+    >
+      <div
+        style={{
+          flexShrink: '0',
+          marginRight: rhythm(1),
+        }}
+      >
         <Gravatar size={60} md5={email} email={name} default="mm" rating="pg" />
       </div>
       <div>
-        <header>
-          <a className="h-card" href={url}>
-            {name}
-          </a>
-          <time dateTime={iso8601Date}>{friendlyDate}</time>
+        <header
+          style={{
+            marginBottom: rhythm(1/2),
+          }}
+        >
+          <a className="h-card" href={url}>{name}</a>
+          <time dateTime={iso8601Date}> on {friendlyDate}</time>
         </header>
         {children}
       </div>
