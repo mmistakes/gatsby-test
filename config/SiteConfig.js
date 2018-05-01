3686f1eb3147b1615d6ab4abd97450ecce76afae
Michael Rose <est.michael@gmail.com>
2018-05-01 14:49:08 -0400

    Add site URL

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
-  siteUrl: `https://mmistakes.github.io`,
+  siteUrl: `https://awesome-lewin-0d1356.netlify.com`,
   title: `Jekyll → Gatsby MVP`,
   author: `Michael Rose`,
   description: `Test conversion of a Jekyll powered site to Gatsby.`,
