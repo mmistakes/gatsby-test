e3f06e8770f73c5111e6ab6a9c5b613525495dde
Michael Rose <est.michael@gmail.com>
2018-05-22 15:15:08 -0400

    Adjust "All Categories" page layout

--- src/pages/category.js
+++ src/pages/category.js
@@ -15,16 +15,103 @@ const CategoriesPage = ({
     },
   },
 }) => (
-  <div>
+  <div
+    className="page"
+    css={{
+      '@media(max-width: 767px)': {
+        marginLeft: '5%',
+        marginRight: '5%',
+      },
+      '@media(min-width: 768px)': {
+        display: 'grid',
+        gridTemplateColumns: '5% 5% 20% 10% 10% 10% 10% 20% 5% 5%',
+        gridTemplateRows: '80px 35%',
+        alignItems: 'end',
+      },
+    }}
+  >
     <Helmet title={`All Categories | ${config.title}`} />
-    <div>
-      <Header />
-      <h1>All Categories</h1>
-      <ul>
+    <Header />
+    <div
+      className="page__title"
+      css={{
+        '@media(min-width: 768px)': {
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignSelf: 'stretch',
+          gridColumn: '2 / span 4',
+          gridRow: '2 / span 2',
+          backgroundColor: 'rgba(255, 255, 255, 0.95)',
+          zIndex: 10,
+        },
+        '@media(min-width: 1024px)': {
+          gridColumn: '2 / span 3',
+        }
+      }}
+    >
+      <h1
+        className="headline"
+        css={{
+          alignSelf: 'flex-start',
+          fontFamily: '"Playfair Display", Times, serif',
+          fontWeight: 700,
+          fontSize: '1.953em',
+          lineHeight: 1.25,
+          textDecoration: 'underline',
+          '@media(min-width: 768px)': {
+            paddingRight: '5%',
+          },
+          '@media(min-width: 1024px)': {
+            fontSize: '2.441em',
+          }
+        }}
+      >
+        All Categories
+      </h1>
+    </div>
+    <div
+      className="page__main"
+      css={{
+        marginTop: '1em',
+        '@media(min-width: 768px)': {
+          gridColumn: '3 / 8',
+          gridRow: '4 / span 1',
+          alignSelf: 'flex-start',
+        },
+        '@media(min-width: 1024px)': {
+          gridColumn: '3 / 7',
+        },
+      }}
+    >
+      <ul
+        css={{
+          listStyle: 'none',
+          margin: 0,
+          padding: 0,
+          '@media(min-width: 768px)': {
+            display: 'grid',
+            gridColumnGap: '2em',
+          },
+          '@media(min-width: 1024px)': {
+            gridTemplateColumns: 'repeat(3, 1fr)',
+          }
+        }}
+      >
         {group.map(category => (
           <li key={category.fieldValue}>
-            <Link to={`/${kebabCase(category.fieldValue)}/`}>
-              {category.fieldValue} ({category.totalCount})
+            <Link
+              css={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                padding: '0.25em 0',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+              to={`/${kebabCase(category.fieldValue)}/`}
+            >
+              <span>{category.fieldValue}</span> <span className="count">{category.totalCount}</span>
             </Link>
           </li>
         ))}
