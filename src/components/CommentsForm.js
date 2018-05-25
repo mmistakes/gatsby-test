6eda4d20337d03fc2cda10ff7f384521f7192ef6
Michael Rose <est.michael@gmail.com>
2018-05-25 15:10:47 -0400

    Adjust Staticman paths

--- src/components/CommentsForm.js
+++ src/components/CommentsForm.js
@@ -9,7 +9,7 @@ const CommentForm = ({ slug }) => {
   return (
     <form method="POST" action="https://api.staticman.net/v2/entry/mmistakes/gatsby-test/master/comments">
       <input name="options[redirect]" type="hidden" value={successPage} />
-      <input name="options[slug]" type="hidden" value={slugDir} />
+      <input name="options[slugDir]" type="hidden" value={slugDir} />
       <input name="fields[slug]" type="hidden" value={slug} />
 
       <label
@@ -34,7 +34,7 @@ const CommentForm = ({ slug }) => {
         }}
       >
         Website
-        <input name="fields[url]" type="text" required />
+        <input name="fields[url]" type="text" />
       </label>
       <label
         css={{
