4a1dbcfa91d2bde14aa144829cfd40aaaef85cb9
Michael Rose <est.michael@gmail.com>
2018-06-04 14:57:58 -0400

    Permanently remove gatsby-remark-code-titles

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,13 +57,6 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          // {
-          //   // IMPORTANT: this must be ahead of other plugins that use code blocks
-          //   resolve: `gatsby-remark-code-titles`,
-          //   options: {
-          //     className: `gatsby-remark-code-title`
-          //   }
-          // },
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
