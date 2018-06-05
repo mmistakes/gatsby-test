2e3f5de809f99fbebaea492acc7d6df78333a560
Michael Rose <est.michael@gmail.com>
2018-06-05 09:01:07 -0400

    Install linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,16 @@ module.exports = {
     "browser": true,
     "es6": true,
   },
+  "root": true,
+  "extends": ["airbnb", "plugin:prettier/recommended"],
+  "settings": {
+    "import/core-modules": ["gatsby"]
+  },
   "plugins": [
     "react",
   ],
   "globals": {
-    "graphql": false,
+    "graphql": true,
   },
   "parserOptions": {
     "sourceType": "module",
@@ -15,5 +20,16 @@ module.exports = {
       "experimentalObjectRestSpread": true,
       "jsx": true,
     },
+  },
+  "rules": {
+    "linebreak-style": 0,
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        "components": ["Link"],
+        "specialLink": ["to"],
+        "aspects": ["noHref", "invalidHref", "preferButton"]
+      }
+    ]
   }
 }
