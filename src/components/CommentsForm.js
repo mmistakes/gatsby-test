baf531b0068898f953fc8c632623c93577a160b8
Michael Rose <est.michael@gmail.com>
2018-06-06 16:20:32 -0400

    Adjust comment form layout

--- src/components/CommentsForm.js
+++ src/components/CommentsForm.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import config from '../../config/SiteConfig'
+import presets from '../utils/presets'
+import fonts from '../utils/fonts'
 
 // Use the `path` (frontmatter) as the identifier for the post.
 const CommentForm = ({ slug }) => {
@@ -7,53 +9,150 @@ const CommentForm = ({ slug }) => {
   const successPage = `${config.siteUrl}/comment-success`
 
   return (
-    <form
-      method="POST"
-      action="https://api.staticman.net/v2/entry/mmistakes/gatsby-test/master/comments"
-    >
-      <input name="options[redirect]" type="hidden" value={successPage} />
-      <input name="options[parent]" type="hidden" value={slug} />
-      <input name="options[slug]" type="hidden" value={slugDir} />
-
-      <label
-        htmlFor="name"
+    <div>
+      <h2>Leave a Comment</h2>
+      <form
+        method="POST"
+        action="https://api.staticman.net/v2/entry/mmistakes/gatsby-test/master/comments"
         css={{
-          display: 'block',
+          position: 'relative',
+          maxWidth: '100%',
         }}
       >
-        Name
-        <input id="name" name="fields[name]" type="text" required />
-      </label>
-      <label
-        htmlFor="email"
-        css={{
-          display: 'block',
-        }}
-      >
-        E-mail
-        <input id="email" name="fields[email]" type="email" required />
-      </label>
-      <label
-        htmlFor="website"
-        css={{
-          display: 'block',
-        }}
-      >
-        Website
-        <input id="website" name="fields[url]" type="text" />
-      </label>
-      <label
-        htmlFor="message"
-        css={{
-          display: 'block',
-        }}
-      >
-        Message
-        <textarea id="message" name="fields[message]" required />
-      </label>
-
-      <button type="submit">Comment</button>
-    </form>
+        <input name="options[redirect]" type="hidden" value={successPage} />
+        <input name="options[parent]" type="hidden" value={slug} />
+        <input name="options[slug]" type="hidden" value={slugDir} />
+        <div
+          css={{
+            [presets.lgUp]: {
+              display: 'flex',
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              marginBottom: '1em',
+            },
+          }}
+        >
+          <label
+            htmlFor="name"
+            css={{
+              display: 'block',
+              margin: '0 0 0.25em 0',
+              fontFamily: fonts.monospace,
+              textTransform: 'uppercase',
+            }}
+          >
+            Name
+            <input
+              id="name"
+              name="fields[name]"
+              type="text"
+              required
+              css={{
+                display: 'block',
+                margin: '0',
+                padding: '0.5em',
+                verticalAlign: 'top',
+                overflow: 'visible',
+                fontFamily: fonts.sansSerif,
+                fontSize: '1em',
+                background: '#fff',
+                border: '1px solid rgba(0,0,0, 0.25)',
+                borderRadius: '0.25em',
+              }}
+            />
+          </label>
+          <label
+            htmlFor="email"
+            css={{
+              display: 'block',
+              margin: '0 0 0.25em 0',
+              fontFamily: fonts.monospace,
+              textTransform: 'uppercase',
+            }}
+          >
+            E-mail
+            <input
+              id="email"
+              name="fields[email]"
+              type="email"
+              required
+              css={{
+                display: 'block',
+                margin: '0',
+                padding: '0.5em',
+                verticalAlign: 'top',
+                overflow: 'visible',
+                fontFamily: fonts.sansSerif,
+                fontSize: '1em',
+                background: '#fff',
+                border: '1px solid rgba(0,0,0, 0.25)',
+                borderRadius: '0.25em',
+              }}
+            />
+          </label>
+          <label
+            htmlFor="website"
+            css={{
+              display: 'block',
+              margin: '0 0 0.25em 0',
+              fontFamily: fonts.monospace,
+              textTransform: 'uppercase',
+            }}
+          >
+            Website
+            <input
+              id="website"
+              name="fields[url]"
+              type="text"
+              css={{
+                display: 'block',
+                margin: '0',
+                padding: '0.5em',
+                verticalAlign: 'top',
+                overflow: 'visible',
+                fontFamily: fonts.sansSerif,
+                fontSize: '1em',
+                background: '#fff',
+                border: '1px solid rgba(0,0,0, 0.25)',
+                borderRadius: '0.25em',
+              }}
+            />
+          </label>
+        </div>
+        <div css={{ marginBottom: '1em' }}>
+          <label
+            htmlFor="message"
+            css={{
+              display: 'block',
+              margin: '0 0 0.25em 0',
+              fontFamily: fonts.monospace,
+              textTransform: 'uppercase',
+            }}
+          >
+            Message
+            <textarea
+              id="message"
+              name="fields[message]"
+              rows="6"
+              required
+              css={{
+                display: 'block',
+                margin: '0',
+                padding: '0.5em',
+                verticalAlign: 'top',
+                overflow: 'visible',
+                fontFamily: fonts.sansSerif,
+                fontSize: '1em',
+                background: '#fff',
+                border: '1px solid rgba(0,0,0, 0.25)',
+                borderRadius: '0.25em',
+              }}
+            />
+          </label>
+        </div>
+        <button type="submit">Comment</button>
+      </form>
+    </div>
   )
 }
 
