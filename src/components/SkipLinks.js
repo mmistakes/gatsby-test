6cf934f3dbe1a1bb4664e4333a254a4b0ab1640c
Michael Rose <est.michael@gmail.com>
2018-06-29 15:15:21 -0400

    Add skip links component

---
+++ src/components/SkipLinks.js
@@ -0,0 +1,24 @@
+import Link from 'gatsby-link'
+import React from 'react'
+
+class SkipLinks extends React.Component {
+  render() {
+    return (
+      <Link
+        to="#main"
+        css={{
+          position: 'absolute',
+          top: '-2em',
+          right: '0.7rem',
+          '&:focus': {
+            top: '0.75rem',
+          }
+        }}
+      >
+        skip to content
+      </Link>
+    )
+  }
+}
+
+export default SkipLinks
