f162c0e30d6af76af1da7df298a3dd47dc362ac4
Michael Rose <est.michael@gmail.com>
2018-07-24 14:58:58 -0400

    Adjust `next` and `previous`

--- gatsby-node.js
+++ gatsby-node.js
@@ -122,9 +122,8 @@ exports.createPages = ({ graphql, actions }) => {
 
         // Create post pages
         _.each(posts, (post, index) => {
-          const previous =
-            index === posts.length - 1 ? false : posts[index + 1].node
-          const next = index === 0 ? false : posts[index - 1].node
+          const previous = index === posts.length - 1 ? null : posts[index + 1].node
+          const next = index === 0 ? null : posts[index - 1].node
 
           createPage({
             path: post.node.fields.slug,
